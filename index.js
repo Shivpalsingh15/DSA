314304166e8cc9e56da5173a904dea814f8f359b
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2024-05-01 16:12:11 +0530

    cube of number

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Q 1 Print "Masai School" in the console followed by "A Transformation in Education" in next line
 
-console.log("Masai School \nA Transformation in Education")
+// console.log("Masai School \nA Transformation in Education")
 
+// ===========================================================================================================================================
 
 // Q 2 Define a variable called name with your Name as the assigned value
 // Print the value stored in the variable name
@@ -11,9 +12,37 @@ console.log("Masai School \nA Transformation in Education")
 // Print the value stored in the variable name
 
 
-let name = 'Shivpal';
-console.log('name:', name)
-name = "Uday Singh Chauhan"
-console.log('name:', name)
-name = "Sundar bai Singh Chauhan"
-console.log('name:', name)
+// let name = 'Shivpal';
+// console.log('name:', name)
+// name = "Uday Singh Chauhan"
+// console.log('name:', name)
+// name = "Sundar bai Singh Chauhan"
+// console.log('name:', name)
+
+// ===========================================================================================================================================
+
+
+// Q.3 Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+
+// let Myname = "Shivpal";
+// let age = 23;
+// console.log('Myname:', Myname, 'age:', age)
+// console.log(typeof(Myname), typeof(age))
+
+
+// ===========================================================================================================================================
+
+
+// Q.4  cubeOfNumber
+
+// function cubeOfNumber(N) {
+//     console.log(N*N*N)
+// }
+
+// cubeOfNumber(2)
+
+
+// ===========================================================================================================================================
