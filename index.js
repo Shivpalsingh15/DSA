6cb15360599791ed758f7f28441ad4feae70d76d
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2024-05-01 16:00:24 +0530

    Storing names

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Q 1 Print "Masai School" in the console followed by "A Transformation in Education" in next line
 
 console.log("Masai School \nA Transformation in Education")
+
+
+// Q 2 Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+
+let name = 'Shivpal';
+console.log('name:', name)
+name = "Uday Singh Chauhan"
+console.log('name:', name)
+name = "Sundar bai Singh Chauhan"
+console.log('name:', name)
